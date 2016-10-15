616c23ed6b0335bf6c75e70ffb918cd41f3a0d2f
陈宇峰 <yufengcode@gmail.com>
2016-10-15 14:05:09 +0800

    init

--- BitAddAlgorithm/main.cpp
+++ BitAddAlgorithm/main.cpp
@@ -8,8 +8,42 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  使用位运算实现加法
+ */
+
+int BitAdd(int a, int b);
+
+
+int main(int argc, const char * argv[])
+{
+    int result = BitAdd(4, 4);
+    cout << result;
+
     return 0;
 }
+
+int BitAdd(int a, int b)
+{
+    int result; // 结果
+
+    result = a ^ b; // 没有进位
+    int isFlow = a & b; // 判断有没有进位
+    while (isFlow)
+    {
+        int temp_a = result;
+        int temp_b = isFlow;
+
+        result = temp_a ^ (temp_b << 1);
+        isFlow = temp_a & temp_b;
+    }
+
+    return result;
+}
+
+
+
+
+
